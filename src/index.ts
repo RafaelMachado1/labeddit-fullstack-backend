e47594c7452bd99cf8d70a3e142b7701bb12706e
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-06-19 15:53:42 -0300

    routes-concluido

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { userRouter } from './router/userRouter'
+import { postRouter } from './router/postRouter'
+import { commentRouter } from './router/commentRouter'
 
 dotenv.config()
 
@@ -13,8 +16,15 @@ app.listen(Number(process.env.PORT) || 3003, () => {
     console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`)
 })
 
+
 // routers das entidades
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
+app.use("/comments", commentRouter)
+
 
 app.get("/ping", (req, res) => {
     res.send("Pong!")
 })
+
+
